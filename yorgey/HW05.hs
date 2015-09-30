5fe93130ff8c20beb8a007282e49eca32bd84c02
Balachandran Chandrasekharan <balachandran.c@gmail.com>
2015-09-30 13:36:39 +0530

    hw5

--- yorgey/HW05.hs
+++ yorgey/HW05.hs
@@ -12,6 +12,7 @@ import qualified Data.Map.Strict as Map
 import Parser
 
 import Data.Bits (xor)
+import Data.List (sortBy)
 
 -- Exercise 1 -----------------------------------------
 
@@ -24,37 +25,56 @@ getSecret mFile oFile = do
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey secret outFilename = do
+  encrypted <- BS.readFile $ outFilename ++ ".enc"
+  let decrypted = BS.pack $ BS.zipWith xor ( BS.cycle secret ) encrypted
+  BS.writeFile outFilename decrypted
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
-parseFile = undefined
+parseFile filename = do
+  contents <- BS.readFile filename
+  return $ decode contents
 
 -- Exercise 4 -----------------------------------------
 
 getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
-getBadTs = undefined
+getBadTs victimList transactionData = do
+  (Just victimTids) <- parseFile victimList ::  IO (Maybe [TId])
+  (Just transactions) <- parseFile transactionData :: IO (Maybe [Transaction])
+  return $ Just $ filter (\x -> ( tid x ) `elem` victimTids ) transactions 
 
 -- Exercise 5 -----------------------------------------
 
 getFlow :: [Transaction] -> Map String Integer
-getFlow = undefined
+getFlow transactions = let  tuples = concatMap (\t -> [( from t, -(amount t)), ( to t, (amount t ) )]) transactions
+                            in foldr ( \(k,v) -> Map.insertWith (+) k v ) Map.empty tuples
 
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
-getCriminal = undefined
+getCriminal candidateMap = fst $ foldr (\(name1, value1) (name2,value2) -> if value1 > value2 then (name1,value1) else (name2,value2)) ("", toInteger (minBound::Int )) $ Map.toList candidateMap
 
 -- Exercise 7 -----------------------------------------
 
 undoTs :: Map String Integer -> [TId] -> [Transaction]
-undoTs = undefined
-
+undoTs candidateMap tids = let  ( payers, payees ) =  Map.partition (>0) candidateMap
+                                payerList = sortByValue payers
+                                payeeList = sortByValue $ fmap abs payees
+                              in createTransactions payerList payeeList tids
+                              where   sortByValue cmap = sortBy ( \(_,v1) (_,v2) -> compare v2 v1 ) $ Map.toList cmap
+                                      createTransactions [] _ _ = []
+                                      createTransactions _ [] _ = []
+                                      createTransactions _ _ [] = []
+                                      createTransactions ((prName,prValue):payers) ((peName,peValue):payees) (t:tids')
+                                        | prValue > peValue = ( Transaction { from = prName, to = peName, amount = peValue, tid = t } ) : createTransactions ((prName,prValue-peValue):payers) payees tids'
+                                        | prValue < peValue = ( Transaction { from = prName, to = peName, amount = prValue, tid = t } ) : createTransactions payers ((peName,peValue-prValue):payees) tids'
+                                        | prValue == peValue= ( Transaction { from = prName, to = peName, amount = prValue, tid = t } ) : createTransactions payers payees tids' 
 -- Exercise 8 -----------------------------------------
 
 writeJSON :: ToJSON a => FilePath -> a -> IO ()
-writeJSON = undefined
+writeJSON outFilename transactions = BS.writeFile outFilename $ encode transactions
 
 -- Exercise 9 -----------------------------------------
 
